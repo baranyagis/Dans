3effad71dc3a94573f001776c3be3735036b8638
Baran Yağış Kızmaz <baranyagiskizmaz2@gmail.com>
2020-03-04 15:58:00 +0300

    MARS Ornek

--- Dans/Form1.Designer.cs
+++ Dans/Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvEslesme = new System.Windows.Forms.DataGridView();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.clmKisi1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmKisi2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEslesme)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEslesme
+            // 
+            this.dgvEslesme.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvEslesme.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvEslesme.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEslesme.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmKisi1,
+            this.clmKisi2});
+            this.dgvEslesme.Location = new System.Drawing.Point(12, 46);
+            this.dgvEslesme.Name = "dgvEslesme";
+            this.dgvEslesme.Size = new System.Drawing.Size(563, 392);
+            this.dgvEslesme.TabIndex = 0;
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(12, 4);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(563, 36);
+            this.btnListele.TabIndex = 1;
+            this.btnListele.Text = "LİSTELE";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // clmKisi1
+            // 
+            this.clmKisi1.HeaderText = "Kişi 1 ";
+            this.clmKisi1.Name = "clmKisi1";
+            // 
+            // clmKisi2
+            // 
+            this.clmKisi2.HeaderText = "Kişi 2";
+            this.clmKisi2.Name = "clmKisi2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(587, 450);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.dgvEslesme);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEslesme)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvEslesme;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmKisi1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmKisi2;
+        private System.Windows.Forms.Button btnListele;
     }
 }
 
--- Dans/Form1.cs
+++ Dans/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,35 @@ namespace Dans
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection("Server=.; database=AralikIskurDb; uid=sa; pwd=123; MultipleActiveResultSets=True;");
+        
         public Form1()
         {
             InitializeComponent();
+            con.Open();
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            dgvEslesme.Rows.Clear();
+            SqlCommand cmd = new SqlCommand("SELECT KisiAd FROM Ogrenciler order by newid()", con);
+            SqlCommand cmd2 = new SqlCommand("SELECT KisiAd FROM Ogrenciler order by newid() desc", con);
+
+            var dr1 = cmd.ExecuteReader();
+            var dr2 = cmd2.ExecuteReader();
+
+            while(dr1.Read())
+            {
+                dr2.Read();
+                dgvEslesme.Rows.Add(dr1["KisiAd"], dr2["KisiAd"]);
+            }
+            dr1.Close();
+            dr2.Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            con.Close();
         }
     }
 }
